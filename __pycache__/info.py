6df53797c4a15e3492b774d85c0f7b9e69e4f6b0
EvanMamun10 <87881009+EvanMamun10@users.noreply.github.com>
2026-02-20 14:58:28 -0500

    Add files via upload

---
+++ __pycache__/info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm Evan Mamun. I am a student at Georgia Tech and I work for NPI Consulting. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/evanmamun/"
+my_github_url = "https://github.com/EvanMamun10"
+my_email_address = "evanmamun1@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Business', 
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '3.0'
+}
+course_data = {
+    "code":["CS 1301", "MGT 3102", "MGT 3501", "HIST 2112,"], 
+    "names":["Intro to CS", "Human Resources", "Operations Managment", "U.S History"], 
+    "semester_taken":["4th", "4th", "4th", "4th"],
+    "skills":["Python is not my favorite snake anymore", "Treat others well, especially in the Office!", "Supply Chains are everything", "I think Kennedy is my favorite! "],
+    }
+experience_data = {
+    "Intern at TWO Capital Partners": (["Conduced Financial Analysis on Real Estate Properties",
+                                                                          "- Over 200 site visits", "- Made the partners proud"],"Images/twocap.jpeg"),
+    "Data Intern at Center for Med":(["- Composed and warehoused patient data",
+                                                           "- Improved scope of care by finding gaps in doctor check-ups"],"Images/med.jpeg"),
+    "Private Tutor":(["-Educated students in all four test sections, leading to an on-average score improvement of 18%"],"Images/act.png")
+
+}
+
+projects_data = {
+    "Model": "$70M core-plus residential acquisition with condo’d retail; built IRR and waterfall models to evaluate returns",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸",
+    "Bengali": "🇧🇩",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Bengali": "Conversational"
+}
+leadership_data = {
+    "Member of Phi Gamma Delta": (["Helping plan social and philanthropic Events"],"Images/fiji.jpg"),
+
+}
+activity_data={
+    "IM Basketball": ["- Strong rotation player", 
+            "- Provided assistance off the bench"]
+}
