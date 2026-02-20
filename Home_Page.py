c20d1a3cf3d675929335ef953500d73031679362
EvanMamun10 <87881009+EvanMamun10@users.noreply.github.com>
2026-02-20 18:38:27 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section X")
+st.subheader("Name")
+
+
+
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. Homepage
+2. Portfolio
+3. Quiz
+
+""")
+
